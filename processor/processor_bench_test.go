c6329814e2a48847707dcc131d326b9dda461457
minodisk <daisuke.mino@gmail.com>
2016-12-27 23:29:47 +0900

    Lock mutex for processing
    Former-commit-id: dfb0c65c07e5eb4dd871c27ba793154ffe560748

--- processor/processor_bench_test.go
+++ processor/processor_bench_test.go
@@ -1,51 +1,78 @@
 package processor_test
 
 import (
-	"bytes"
-	"os"
 	"testing"
 
-	"github.com/go-microservices/resizer/fetcher"
-	"github.com/go-microservices/resizer/option"
 	"github.com/go-microservices/resizer/processor"
 	"github.com/go-microservices/resizer/storage"
 )
 
-func BenchmarkProcess(b *testing.B) {
-	if err := fetcher.Init(); err != nil {
+type NopWriter struct{}
+
+func (w NopWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func BenchmarkHuge(b *testing.B) {
+	if err := process("../fixtures/huge.jpg"); err != nil {
 		b.Fatal(err)
 	}
-	o, err := option.New(os.Args[1:])
+}
+
+func BenchmarkLarge(b *testing.B) {
+	if err := process("../fixtures/large.jpg"); err != nil {
+		b.Fatal(err)
+	}
+}
+
+func BenchmarkMedium(b *testing.B) {
+	if err := process("../fixtures/medium.jpg"); err != nil {
+		b.Fatal(err)
+	}
+}
+
+func BenchmarkSmall(b *testing.B) {
+	if err := process("../fixtures/small.jpg"); err != nil {
+		b.Fatal(err)
+	}
+}
+
+// func BenchmarkMemory(b *testing.B) {
+// 	go func() {
+// 		var mem runtime.MemStats
+// 		for {
+// 			runtime.ReadMemStats(&mem)
+// 			fmt.Printf("%d\t%d\t%d\t%d\n", mem.Alloc, mem.TotalAlloc, mem.HeapAlloc, mem.HeapSys)
+// 			time.Sleep(time.Second * 1)
+// 		}
+// 	}()
+//
+// 	wg := new(sync.WaitGroup)
+// 	for i := 0; i < 100; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			if err := process(b, "../fixtures/medium.jpg"); err != nil {
+// 				b.Fatal(err)
+// 			}
+// 			wg.Done()
+// 		}()
+// 	}
+// 	wg.Wait()
+// }
+
+func process(path string) error {
+	p := processor.New()
+	i, err := storage.NewImage(storage.Input{
+		URL:   "http://example.com/test.jpg",
+		Width: 800,
+	})
 	if err != nil {
-		b.Fatal(err)
+		return err
 	}
 
-	for i := 0; i < b.N; i++ {
-		o, err := storage.NewImage(map[string][]string{
-			"url":    []string{""},
-			"width":  []string{"800"},
-			"height": []string{"0"},
-		}, o.Hosts)
-		if err != nil {
-			b.Fatalf("%s", err)
-		}
-
-		path, err := fetcher.Fetch(o.ValidatedURL)
-		if err != nil {
-			b.Fatalf("%s", err.Error())
-		}
-
-		var bs []byte
-		w := bytes.NewBuffer(bs)
-		p := processor.New()
-
-		image, err := p.Preprocess(path)
-		if err != nil {
-			b.Fatalf("%s", err.Error())
-		}
-
-		if _, err := p.Process(image, w, o); err != nil {
-			b.Fatalf("%s", err.Error())
-		}
+	var w NopWriter
+	if _, err = p.Process(path, &w, i); err != nil {
+		return err
 	}
+	return nil
 }
