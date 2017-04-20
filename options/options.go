d6167ba2c723ca8ed5dac758bfe88530108b8f1c
minodisk <daisuke.mino@gmail.com>
2017-04-20 17:19:59 +0900

    Add object prefix option

--- options/options.go
+++ options/options.go
@@ -7,6 +7,7 @@ type Options struct {
 	Bucket             string
 	DataSourceName     string
 	MaxHTTPConnections int
+	ObjectPrefix       string
 	Port               int
 	ServiceAccount     ServiceAccount
 	Verbose            bool
@@ -22,8 +23,11 @@ func Parse(args []string) (Options, error) {
 	           $ resizer -host a.com -host b.com`)
 	fs.StringVar(&o.Bucket, "bucket", "", `Bucket name of Google Cloud Storage to upload the resized image.`)
 	fs.StringVar(&o.DataSourceName, "dsn", "", `Data source name of database to store resizing information.`)
-	fs.IntVar(&o.MaxHTTPConnections, "connections", 2, `Max simultaneous connections to be accepted by server.`)
-	fs.IntVar(&o.Port, "port", 80, `TCP address to listen on.
+	fs.IntVar(&o.MaxHTTPConnections, "connections", 0, `Max simultaneous connections to be accepted by server.
+         When 0 or less is specified, the number of connections isn't limited.
+         `)
+	fs.StringVar(&o.ObjectPrefix, "prefix", "resized/", ``)
+	fs.IntVar(&o.Port, "port", 80, `Port to be listened.
          `)
 	fs.Var(&o.ServiceAccount, "account", `Path to the file of Google service account JSON.`)
 	fs.BoolVar(&o.Verbose, "verbose", false, `Verbose output.
