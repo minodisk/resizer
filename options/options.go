e3a2f367a0d9bdf280695537b80faee581a22e54
minodisk <daisuke.mino@gmail.com>
2017-04-21 00:40:32 +0900

    Add tests for options
    Former-commit-id: 785bbbbde992e0b2f7da40a61cd2564962157350

--- options/options.go
+++ options/options.go
@@ -18,20 +18,18 @@ func Parse(args []string) (Options, error) {
 
 	fs := flag.NewFlagSet("resizer", flag.ContinueOnError)
 	fs.Var(&o.AllowedHosts, "host", `Hosts of the image that is allowed to resize.
-	        When this value isn't specified, all hosts are allowed.
-	        You can set multi hosts with:
-	           $ resizer -host a.com -host b.com`)
+		When this value isn't specified, all hosts are allowed.
+		You can set multi hosts with:
+			$ resizer -host a.com -host b.com`)
 	fs.StringVar(&o.Bucket, "bucket", "", `Bucket name of Google Cloud Storage to upload the resized image.`)
 	fs.StringVar(&o.DataSourceName, "dsn", "", `Data source name of database to store resizing information.`)
 	fs.IntVar(&o.MaxHTTPConnections, "connections", 0, `Max simultaneous connections to be accepted by server.
-         When 0 or less is specified, the number of connections isn't limited.
-         `)
-	fs.StringVar(&o.ObjectPrefix, "prefix", "resized/", ``)
+		When 0 or less is specified, the number of connections isn't limited.`)
+	fs.StringVar(&o.ObjectPrefix, "prefix", "", `Prefix of object name.`)
 	fs.IntVar(&o.Port, "port", 80, `Port to be listened.
-         `)
+		`)
 	fs.Var(&o.ServiceAccount, "account", `Path to the file of Google service account JSON.`)
-	fs.BoolVar(&o.Verbose, "verbose", false, `Verbose output.
-         `)
+	fs.BoolVar(&o.Verbose, "verbose", false, `Verbose output.`)
 
 	return o, fs.Parse(args)
 }
--- options/options_test.go
+++ options/options_test.go
@@ -1,59 +1,57 @@
 package options_test
 
-// func TestLoad(t *testing.T) {
-// 	os, err := option.Load("../fixtures/config.yml")
-// 	if err != nil {
-// 		t.Fatal(errors.Wrap(err, "fail to load config"))
-// 	}
-//
-// 	if a, e := len(os), 2; a != e {
-// 		t.Errorf("the length of options expected %d, but actual %d", e, a)
-// 	}
-//
-// 	type Case struct {
-// 		Key    string
-// 		Option option.NewOption
-// 	}
-//
-// 	cases := []Case{
-// 		{
-// 			Key: "*",
-// 			Option: option.NewOption{
-// 				MaxHTTPConnections: 7,
-// 				GoogleCloud: option.GoogleCloud{
-// 					ProjectID:         "syoya-test",
-// 					ServiceAccount:    "/secret/gcloud.json",
-// 					StorageBucketName: "resizer",
-// 				},
-// 				MySQL: option.MySQL{
-// 					DataSourceName: "root:@tcp(mysql:3306)/resizer?charset=utf8&parseTime=True",
-// 				},
-// 			},
-// 		},
-// 		{
-// 			Key: "foo.bar",
-// 			Option: option.NewOption{
-// 				MaxHTTPConnections: 7,
-// 				GoogleCloud: option.GoogleCloud{
-// 					ProjectID:         "foo-bar",
-// 					ServiceAccount:    "/foo/bar.json",
-// 					StorageBucketName: "foo",
-// 				},
-// 				MySQL: option.MySQL{
-// 					DataSourceName: "foo:bar@tcp(mysql:3306)/baz",
-// 				},
-// 			},
-// 		},
-// 	}
-//
-// 	for _, c := range cases {
-// 		o, ok := os[c.Key]
-// 		if !ok {
-// 			t.Errorf("the key `%s` expected existing, but actual not", c.Key)
-// 			continue
-// 		}
-// 		if a, e := o, c.Option; !reflect.DeepEqual(a, e) {
-// 			t.Errorf("option is expected:\n%+v\nbut actual:\n%+v\n", e, a)
-// 		}
-// 	}
-// }
+import (
+	"reflect"
+	"testing"
+
+	"github.com/minodisk/resizer/options"
+)
+
+func TestParse(t *testing.T) {
+	t.Parallel()
+
+	t.Run("failure", func(t *testing.T) {
+		t.Parallel()
+		for _, c := range []struct {
+			name string
+			args []string
+		}{
+			{
+				"undefined flag",
+				[]string{
+					"-xxx",
+				},
+			},
+		} {
+			c := c
+			t.Run(c.name, func(t *testing.T) {
+				t.Parallel()
+				_, err := options.Parse(c.args)
+				if err == nil {
+					t.Errorf("should return error")
+				}
+			})
+		}
+	})
+
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+		for _, c := range []struct {
+			name string
+			args []string
+			want options.Options
+		}{} {
+			c := c
+			t.Run(c.name, func(t *testing.T) {
+				t.Parallel()
+				got, err := options.Parse(c.args)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !reflect.DeepEqual(got, c.want) {
+					t.Errorf("\n got: %+v\nwant: %+v", got, c.want)
+				}
+			})
+		}
+	})
+}
